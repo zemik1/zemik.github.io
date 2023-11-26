5d093deb00b5e28216fae0d963b8f96a838bf1d0
zemik1 <danilzemik73@gmail.com>
2023-11-26 06:10:01 +0500

    Updates

---
+++ precache-manifest.88341e9f3a8292b94ff0ca878dc5ba7d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977da5f9ae377724d6bfba1f3ee667a6",
+    "url": "/zemik.github.io/index.html"
+  },
+  {
+    "revision": "ea8de10caa7050d8263e",
+    "url": "/zemik.github.io/static/css/main.bd2807f4.chunk.css"
+  },
+  {
+    "revision": "fbda95f89e5f8eb70be3",
+    "url": "/zemik.github.io/static/js/2.90a2cde4.chunk.js"
+  },
+  {
+    "revision": "ea8de10caa7050d8263e",
+    "url": "/zemik.github.io/static/js/main.9a53a916.chunk.js"
+  },
+  {
+    "revision": "63ffac8772e6c67545f1",
+    "url": "/zemik.github.io/static/js/runtime~main.f4245b1a.js"
+  },
+  {
+    "revision": "c5787617d399bcbf94789945a15f86b3",
+    "url": "/zemik.github.io/static/media/gitHub.c5787617.svg"
+  },
+  {
+    "revision": "6f3e199351c1a9e63fa72c93d9a8386f",
+    "url": "/zemik.github.io/static/media/header-bg.6f3e1993.png"
+  },
+  {
+    "revision": "336dd870b18a0cbbcc0498151e977c80",
+    "url": "/zemik.github.io/static/media/instagram.336dd870.svg"
+  },
+  {
+    "revision": "c0e1ca3d6c5df5bd7dbad48c0f1d5cb2",
+    "url": "/zemik.github.io/static/media/moon.c0e1ca3d.svg"
+  },
+  {
+    "revision": "ccaa7d239372739f1894437fc06843a9",
+    "url": "/zemik.github.io/static/media/sun.ccaa7d23.svg"
+  },
+  {
+    "revision": "53e5371356779a6147cac987a99963c0",
+    "url": "/zemik.github.io/static/media/vk.53e53713.svg"
+  }
+]);
